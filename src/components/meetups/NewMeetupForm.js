220c980a842583b8830066a3af04ba572332f3af
mahdi chihi <mahdichihi95@gmail.com>
2023-02-02 10:57:32 +0100

    Getting User Input & Handling Form Submission

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -1,28 +1,74 @@
+import { useRef } from "react";
+
 import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 
 function NewMeetupForm() {
+    const titleInputRef = useRef();
+    const imageInputRef = useRef();
+    const addressInputRef = useRef();
+    const descriptionInputRef = useRef();
+
+    function submitHandler(event) {
+        // to prevent browser from sending a HTTP request
+        event.preventDefault();
+
+        const enteredTitle = titleInputRef.current.value;
+        const enteredImage = imageInputRef.current.value;
+        const enteredAddress = addressInputRef.current.value;
+        const enteredDescription = descriptionInputRef.current.value;
+
+        const meetupData = {
+            title: enteredTitle,
+            image: enteredImage,
+            address: enteredAddress,
+            description: enteredDescription,
+        };
+
+        console.log(meetupData);
+    }
+
     return (
         <Card>
-            <form className={classes.form}>
+            <form className={classes.form} onSubmit={submitHandler}>
                 <div className={classes.control}>
                     <label htmlFor="title">Meetup Title</label>
-                    <input type="text" id="title" required></input>
+                    <input
+                        type="text"
+                        id="title"
+                        required
+                        ref={titleInputRef}
+                    ></input>
                 </div>
 
                 <div className={classes.control}>
                     <label htmlFor="image">Meetup Image</label>
-                    <input type="url" id="image" required></input>
+                    <input
+                        type="url"
+                        id="image"
+                        required
+                        ref={imageInputRef}
+                    ></input>
                 </div>
 
                 <div className={classes.control}>
                     <label htmlFor="address">Address</label>
-                    <input type="text" id="address" required></input>
+                    <input
+                        type="text"
+                        id="address"
+                        required
+                        ref={addressInputRef}
+                    ></input>
                 </div>
 
                 <div className={classes.control}>
                     <label htmlFor="description">Description</label>
-                    <textarea id="description" required rows={5}></textarea>
+                    <textarea
+                        id="description"
+                        required
+                        rows={5}
+                        ref={descriptionInputRef}
+                    ></textarea>
                 </div>
 
                 <div className={classes.actions}>
